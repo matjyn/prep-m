bcebca2949fe51fc041c8ee0e4cf412cea581dcd
matinswe <matinmammadli0420@gmail.com>
2026-01-27 12:57:24 +0400

    changes

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router";
 import { Header } from "../components/layout/Header";
 import { useUserStore } from "../stores/user";
+import { Modal } from "../components/ui/Modal/Modal";
+import { UserInfo } from "../components/common/UserInfo";
 
 const MainLayout = () => {
   const user = useUserStore((state) => state.user);
@@ -9,7 +11,21 @@ const MainLayout = () => {
     <div className="p-3">
       <Header />
 
-      <div>{user ? <Outlet /> : <div>You must login first!</div>}</div>
+      <div className="py-3">
+        {user ? (
+          <Outlet />
+        ) : (
+          <Modal
+            isOpen={true}
+            onClose={() => {}}
+          >
+            <div className="flex flex-column justify-center align-center gap-2 p-4">
+              <p className="text-center m-0">You must login first!</p>
+              <UserInfo />
+            </div>
+          </Modal>
+        )}
+      </div>
     </div>
   );
 };
