50cdc58f88a96a0440863c471bc39d5513251b5a
matinswe <matinmammadli0420@gmail.com>
2026-02-03 15:40:40 +0400

    changes

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,15 +1,11 @@
 import { API_URL } from "../config/constants";
-import { getOrder } from "../helpers/getOrders";
 import type { Asset, AssetsQueryParams } from "../types/api/assets";
 
 async function fetchAssets(params: AssetsQueryParams = {}): Promise<Asset[]> {
-  const { limit = 10, offset = 0, sortBy, sortOrder } = params;
+  const { limit = 10, offset = 0 } = params;
   const page = Math.floor(offset / limit) + 1;
-  const order = getOrder(sortBy, sortOrder);
 
-  const res = await fetch(
-    `${API_URL}?vs_currency=usd&order=${order}&per_page=${limit}&page=${page}`,
-  );
+  const res = await fetch(`${API_URL}?vs_currency=usd&per_page=${limit}&page=${page}`);
 
   if (!res.ok) throw new Error("Failed to fetch assets");
 
